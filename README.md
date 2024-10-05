41731cdc1d834c242457d62b06cd614a18c4e812
Telmo André Azevedo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2024-10-06 00:44:28 +0100

    Piece (add)

---
+++ README.md
@@ -0,0 +1,3 @@
+# ScaLess - **Scal**a Ch**Ess**
+
+using Scala to deploy a funny yet not so fresh Chess project
---
+++ src/main/scala/scaless/piece/Piece.scala
@@ -0,0 +1,143 @@
+package scaless.piece
+
+/**
+ * this is my representation of a chess piece
+ * @param color this is the color of the piece, i.e., "White" or "Black"
+ * */
+abstract class Piece(val color: String):
+  def piece: String           // type of piece
+  def status: String          // i.e., checked, checkmate, stalemate ...
+  def position: (Int, Int)    // 0 <= row < 7 && 0 <= column <= 7
+  def moves: List[(Int, Int)] // set of legal moves
+end Piece
+
+case class King(override val color: String) extends Piece(color):
+  def piece: String = "King"
+  def status: String = "Inactive"
+  def position: (Int, Int) = null
+
+  def moves: List[(Int, Int)] =
+    val currentRow = position._1
+    val currentCol = position._2
+
+    val standardMoves = for
+      row <- (currentRow - 1) to (currentRow + 1)
+      col <- (currentCol - 1) to (currentCol + 1)
+      if row >= 0 && row < 8 // could get coordinates from scaless.board.Board
+      if col >= 0 && col < 8 // could get coordinates from scaless.board.Board
+      if !(row == currentRow && col == currentCol)
+    yield (row, col)
+
+    standardMoves.toList
+    // gotta remove all illegal moves
+  end moves
+end King
+
+case class Queen(override val color: String) extends Piece(color):
+  def piece: String = "Queen"
+  def status: String = "Inactive"
+  def position: (Int, Int) = null
+
+  def moves: List[(Int, Int)] =
+    val currentRow = position._1
+    val currentCol = position._2
+
+    val horizontalMoves = for
+      col <- 0 until 8  // could get coordinates from scaless.board.Board
+      if col != currentCol
+    yield (currentRow, col)
+
+    val verticalMoves = for
+      row <- 0 until 8  // could get coordinates from scaless.board.Board
+      if row != currentRow
+    yield (row, currentCol)
+
+    val diagonalMoves = for
+      offset <- 1 until 8 // could get coordinates from scaless.board.Board
+      (row, col) <- List(
+        (currentRow + offset, currentCol + offset), // down-right
+        (currentRow + offset, currentCol - offset), // down-left
+        (currentRow - offset, currentCol + offset), // up-right
+        (currentRow - offset, currentCol - offset)  // up-left
+      )
+      if row >= 0 && row < 8 // could get coordinates from scaless.board.Board
+      if col >= 0 && col < 8 // could get coordinates from scaless.board.Board
+    yield (row, col)
+
+    val standardMoves = horizontalMoves ++ verticalMoves ++ diagonalMoves
+    standardMoves.toList
+    // gotta remove all illegal moves
+  end moves
+end Queen
+
+case class Knight(override val color: String) extends Piece(color):
+  def piece: String = "Knight"
+  def status: String = "Inactive"
+  def position: (Int, Int) = null
+
+  def moves: List[(Int, Int)] =
+    ???
+  end moves
+end Knight
+
+case class Bishop(override val color: String) extends Piece(color):
+  def piece: String = "Bishop"
+  def status: String = "Inactive"
+  def position: (Int, Int) = null
+
+  def moves: List[(Int, Int)] =
+    val currentRow = position._1
+    val currentCol = position._2
+
+    val standardMoves = for
+      offset <- 1 until 8 // could get coordinates from scaless.board.Board
+      (row, col) <- List(
+        (currentRow + offset, currentCol + offset), // down-right
+        (currentRow + offset, currentCol - offset), // down-left
+        (currentRow - offset, currentCol + offset), // up-right
+        (currentRow - offset, currentCol - offset)  // up-left
+      )
+      if row >= 0 && row < 8 // could get coordinates from scaless.board.Board
+      if col >= 0 && col < 8 // could get coordinates from scaless.board.Board
+    yield (row, col)
+
+    standardMoves.toList
+    // gotta remove all illegal moves
+  end moves
+end Bishop
+
+case class Rook(override val color: String) extends Piece(color):
+  def piece: String = "Rook"
+  def status: String = "Inactive"
+  def position: (Int, Int) = null
+
+  def moves: List[(Int, Int)] =
+    val currentRow = position._1
+    val currentCol = position._2
+
+    val horizontalMoves = for
+      col <- 0 until 8 // could get coordinates from scaless.board.Board
+      if col != currentCol
+    yield (currentRow, col)
+
+    val verticalMoves = for
+      row <- 0 until 8 // could get coordinates from scaless.board.Board
+      if row != currentRow
+    yield (row, currentCol)
+
+    val standardMoves = horizontalMoves ++ verticalMoves
+    standardMoves.toList
+    // gotta remove all illegal moves
+  end moves
+end Rook
+
+case class Pawn(override val color: String) extends Piece(color):
+  def piece: String = "Pawn"
+  def status: String = "Inactive"
+  def position: (Int, Int) = null
+
+  def moves: List[(Int, Int)] =
+    ???
+    // gotta remove all illegal moves
+  end moves
+end Pawn
